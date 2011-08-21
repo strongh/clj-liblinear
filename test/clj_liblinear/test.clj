8e7a09b1d38867f5c959df318c89332ffbe46bad
Homer Strong <homer.strong@gmail.com>
2011-08-21 10:05:46 -0700

    make assertions in tests

--- test/clj_liblinear/test.clj
+++ test/clj_liblinear/test.clj
@@ -1,15 +1,18 @@
 (ns clj-liblinear.test
-  (:use [clj-liblinear.core :only [train predict]]))
+  (:use
+   clojure.test
+   [clj-liblinear.core :only [train predict]]))
 
 
-(let [train-data (concat
-                  (repeatedly 300 #(hash-map :class 0 :f {:x (rand), :y (rand)}))
-                  (repeatedly 300 #(hash-map :class 1 :f {:x (- (rand)), :y (- (rand))})))
-      model (train
-             (map :f train-data)
-             (map :class train-data)
-             :algorithm :l2l2)]
-  
-  [(predict model {:x (rand) :y (rand)})
-   (predict model {:x (- (rand)) :y (- (rand))})])
-;;=> [0 1]
+(deftest simple-classification
+  (let [train-data
+        (concat
+         (repeatedly 300 #(hash-map :class 0 :f {:x (rand), :y (rand)}))
+         (repeatedly 300 #(hash-map :class 1 :f {:x (- (rand)), :y (- (rand))})))
+        model (train
+               (map :f train-data)
+               (map :class train-data)
+               :algorithm :l2l2)]
+    
+    [(is (= 0 (predict model {:x (rand) :y (rand)})))
+     (is (= 1 (predict model {:x (- (rand)) :y (- (rand))})))]))
